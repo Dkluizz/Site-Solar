b41f4758b5e9fdc4ad5ab8fa86817113f88d3da2
tiagomoraes45 <61637890+tiagomoraes45@users.noreply.github.com>
2023-02-26 22:53:11 -0300

    index

--- lib/main/head.php
+++ lib/main/head.php
@@ -2,7 +2,7 @@
 
 
     <meta charset="utf-8">
-    <title>Solartec - Renewable Energy Website Template</title>
+    <title>Atende - Solar</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
