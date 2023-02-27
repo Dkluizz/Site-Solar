e92c3f9afaa0f251bc97ae7811f5ea1a91222926
tiagomoraes45 <61637890+tiagomoraes45@users.noreply.github.com>
2023-02-27 08:54:40 -0300

    cores

--- lib/main/Navbar.php
+++ lib/main/Navbar.php
@@ -37,7 +37,7 @@
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
     <a href="index.php" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
       <img src="img/icone.png" alt="..."  height="100%">&nbsp&nbsp
-      <h3 class="m-0 text-info">ATENDE</h3>&nbsp<h3 class="m-0 text-warning">SOLARES</h3>
+      <h3 class="m-0 text-primary">ATENDE</h3>&nbsp<h3 class="m-0 text-warning">SOLARES</h3>
     </a>
        
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
