aafde5caacebf1103b308a1f1dee2535e62096ea
tiagomoraes45 <61637890+tiagomoraes45@users.noreply.github.com>
2023-02-27 19:36:22 -0300

    Create Footer.php

---
+++ lib/main/Footer.php
@@ -0,0 +1,80 @@
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-white text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Endereço</h5>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Bocaiuva - MG, BR</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>(38) 9 99965-0499</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>atendesolares@gmail.com</p>
+                    <div class="d-flex pt-2">                        
+                        <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>                        
+                        <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Links Rápidos</h5>
+                    <a class="btn btn-link" href="">Sobre Nós</a>
+                    <a class="btn btn-link" href="">Contate-nos</a>
+                    <a class="btn btn-link" href="">Outros Serviços</a>
+                    <a class="btn btn-link" href="">Suporte</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Galeria de Projetos</h5>
+                    <div class="row g-2">
+                        <div class="col-4">
+                            <img class="img-fluid rounded" src="img/gallery-1.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid rounded" src="img/gallery-2.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid rounded" src="img/gallery-3.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid rounded" src="img/gallery-4.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid rounded" src="img/gallery-5.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid rounded" src="img/gallery-6.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+<<<<<<< HEAD
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Newsletter</h5>
+                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                        <input class="form-control border-0 w-50 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                    </div>
+                </div>
+=======
+>>>>>>> 27f0ac1dee834309069aba384b77dc5144f3a5d5
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a href="#">Atende Solares</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        Designed By FT </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+    <script src="//code.jivosite.com/widget/QqqNxGW7Ok" async></script>
+
+
