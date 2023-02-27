cd7eabfe1c8062fadeef80ce3cf4678faf14371d
tiagomoraes45 <61637890+tiagomoraes45@users.noreply.github.com>
2023-02-26 23:46:22 -0300

    LOGO ATENDE SOLARES NOVO

--- lib/main/Navbar.php
+++ lib/main/Navbar.php
@@ -36,7 +36,7 @@
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
         <a href="index.php" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
-            <h2 class="m-0 text-primary">Solartec</h2>
+            <h2 class="m-0 text-primary">ATENDE SOLAR</h2>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
