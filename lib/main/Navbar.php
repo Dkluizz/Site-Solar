da2e863df9e6571b71231b19f90bef35c2a07819
tiagomoraes45 <61637890+tiagomoraes45@users.noreply.github.com>
2023-02-27 11:14:13 -0300

    ajuste logo
    ajuste logo

--- lib/main/Navbar.php
+++ lib/main/Navbar.php
@@ -34,8 +34,8 @@
 
 
     <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-    <a href="index.php" class="navbar-brand d-flex align-items-center  px-5 px-lg-5">
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 ">
+    <a href="index.php" class="navbar-brand d-flex align-items-center  px-2 px-lg-2">
       <img src="img/icone.png" alt="..."  height="100%">&nbsp&nbsp
       <h3 class="m-0 text-primary">ATENDE</h3>&nbsp<h3 class="m-0 text-warning">SOLARES</h3>
     </a>
