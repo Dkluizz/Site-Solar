5f11e38bdfa258c9188b6ba4a5cdf54748a15403
tiagomoraes45 <61637890+tiagomoraes45@users.noreply.github.com>
2023-02-27 10:07:34 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -74,7 +74,7 @@
                 <div class="col-lg-6 pe-lg-0" style="min-height: 400px;">
                     <div class="position-relative h-100">
                         <iframe class="position-absolute w-100 h-100" style="object-fit: cover;"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3205.0563896405997!2d-43.823154647374736!3d-17.111590822141626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sSilvia%20Domingas%20de%20Souza%2C%20114%20-%20Bocai%C3%BAva%2C%20MG%2C%2039390-000!"
                         frameborder="0" allowfullscreen="" aria-hidden="false"
                         tabindex="0"></iframe>
                     </div>
